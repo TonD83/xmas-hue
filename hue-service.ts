ca85ca7d29d7269ab14972d6eefce08866a6b3d2
Ton Dumans <ton.dumans@amis.nl>
2016-12-21 15:48:11 +0100

    - added rate limiting - implemented kue - removed twitter left-overs

--- hue-service.ts
+++ hue-service.ts
@@ -3,16 +3,11 @@ import * as colorstring from 'color-string';
 import * as jsonfile from 'jsonfile';
 import * as hue from 'node-hue-api';
 
-// let TwitterPackage = require('twitter');
-
 let HueApi = hue.HueApi,
     lightState = hue.lightState;
 
 let config = jsonfile.readFileSync('./config.json');
-let twitterConfig = config.twitterConfig;
-
 let hueAPI = new HueApi(config.hueConfig.hostname, config.hueConfig.username);
-// let Twitter = new TwitterPackage(twitterConfig);
 
 let reply = config.reply;
 let disco: Boolean = false;
@@ -29,62 +24,10 @@ class TreeStatus {
     effect: string;
 }
 
-// Twitter.stream('statuses/filter', { track: '#xmashue' }, function (stream) {
-//     stream.on('data', function (tweet) {
-//         log('Text: ' + tweet.text);
-//         log('Username: ' + tweet.user.screen_name);
-
-//         if (tweet.text.includes('rainbow')) {
-//             for (let light of config.hueConfig.lights) {
-//                 rainbowRoad(light);
-//             }
-//         } else {
-//             if (tweet.text.includes('disco')) {
-//                 disco = true;
-//             } else {
-//                 disco = false;
-//             }
-//             let transitionTime = disco ? config.transitionTimeDisco : config.transitionTime;
-//             let colors = getColorsFromTweet(tweet.text);
-//             let timeout = -1 * transitionTime;
-//             for (let color of colors) {
-//                 for (let light of config.hueConfig.lights) {
-//                     let col = { red: color[0], green: color[1], blue: color[2] };
-//                     setTimeout(() => { setLightToColor(light, col) }, timeout);
-//                     timeout += transitionTime;
-//                     log(timeout);
-//                 }
-//             }
-//         }
-
-//         if (reply) {
-//             let name = pokemon.random();
-//             let statusObj = {
-//                 status: "Hi @" + tweet.user.screen_name + ", " + name + " says thanks!",
-//                 in_reply_to_status_id: tweet.id
-//             }
-
-//             Twitter.post('statuses/update', statusObj, function (error, tweetReply, response) {
-//                 if (error) {
-//                     log(error);
-//                 }
-//                 log(tweetReply.text);
-//             });
-//         }
-//     });
-
-//     stream.on('error', function (error) {
-//         log(error);
-//     });
-// });
-
 export function setLightToColor(lightId: number, color: Color) {
-    log(`Setting light id ${lightId} to color: `);
-    log(color);
-
     let state = lightState.create().on().hsb(color.h, color.s, color.b);
     hueAPI.setLightState(lightId, state)
-        .then(displayResult)
+        .then(displaySetLightStateResult)
         .catch(function (e) {
             log(e);
         })
@@ -106,6 +49,12 @@ export function getLightColor(lightId: number) {
     });
 }
 
+export function getLightStatus() {
+    hueAPI.sensors()
+    .then(displayResult)
+    .done();
+}
+
 function getHue(philipsHue) {
     return Math.round((philipsHue.state.hue / ((256 * 256) - 1)) * 360);
 }
@@ -118,8 +67,14 @@ function getBrightness(philipsHue) {
     return Math.round((philipsHue.state.bri / 255) * 100);
 }
 
-let displayResult = function (result) {
-    if (result === "true") {
+let displaySetLightStateResult = function (result) {
+    if (result == true) {
         log('Light succesfully changed state');
+    } else {
+        log(result);
     }
+};
+
+let displayResult = function(result) {
+    log(JSON.stringify(result, null, 2));
 };
--- index.ts
+++ index.ts
@@ -5,25 +5,61 @@ import * as log from 'fancy-log';
 import * as colorstring from 'color-string';
 import * as colorconvert from 'color-convert';
 import * as jsonfile from 'jsonfile';
+import * as kue from 'kue';
+import * as rateLimit from 'express-rate-limit';
 
+let queue = kue.createQueue();
 let app = express();
 let config = jsonfile.readFileSync('./config.json');
 
+/* Kue setup */
+kue.app.listen(3100);
+queue.process('changeLightState', (job, done) => {
+    processLightStateChange(job.data, done);
+});
+
+queue.on('error', (err) => {
+    log('Kue error... ', err);
+});
+
+/* Express setup */
+let apiLimiter = new rateLimit({
+  windowMs: 1*60*60*1000, // 1 hour
+  max: 6,
+  delayMs: 0 // disabled 
+});
+app.use('/api/', apiLimiter);
+
 app.use(bodyParser.json());
 
-app.get('/tree', function (req, res) {
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/api/v1/tree', (req, res) => {
     log('GET Request from address: ' + req.connection.remoteAddress);
+
+    let item = req.headers;
+    if (!item.apikey || !validateApiKey(item.apikey)) return res.status(401).send({ requestStatus: 'GTFO dear Cindy!' });
+
     getTreeStatus().then(treeStatusResponse => {
         res.send(treeStatusResponse);
     })
 });
 
-app.post('/tree', function (req, res) {
+app.post('/api/v1/tree', (req, res) => {
     log('POST Request from address: ' + req.connection.remoteAddress);
 
-    var item = req.body;
-    if (!item.colors) return res.send(400, { requestStatus: 'colors is required' });
-    if (item.colors.length == 0) return res.send(400, { requestStatus: 'you should specify at least 1 color' });
+    let item = req.body;
+    let headers = req.headers;
+    if (!headers.apikey || !validateApiKey(headers.apikey)) return res.status(401).send({ requestStatus: 'GTFO!' });
+    if (!item.colors) return res.status(400).send({ requestStatus: 'colors is required' });
+    if (item.colors.length == 0) return res.status(400).send({ requestStatus: 'you should specify at least 1 color' });
+    if (item.colors.length > 10) {
+        item.colors = item.colors.slice(0,9);
+    }
 
     let colors = item.colors;
     let disco = item.disco;
@@ -38,16 +74,21 @@ app.post('/tree', function (req, res) {
             if (rgbColor) {
                 let hsbColor = colorconvert.rgb.hsv(rgbColor[0], rgbColor[1], rgbColor[2]);
                 color = { h: hsbColor[0], s: hsbColor[1], b: hsbColor[2] };
-                log(`String "${name}" converted to HSB color h:${color.h} s:${color.s} b:${color.b}`);
+                log(`String "${name}" converted to HSB color:`);
                 log(color);
             } else {
-                return res.send(400, { error: `Could not parse color "${name}"` });
+                return res.status(400).send({ error: `Could not parse color "${name}"` });
             }
         }
         for (let light of config.hueConfig.lights) {
-            setTimeout(() => {
-                hueService.setLightToColor(light, color)
-            }, delay);
+            let stateChangeJob = queue.create('changeLightState', {
+                title: 'light state change job',
+                lightId: light,
+                color: color
+            }).delay(delay)
+                .save((err) => {
+                    if (err) log(err);
+                });
         }
         delay += delayTime;
     }
@@ -57,7 +98,7 @@ app.post('/tree', function (req, res) {
 
 });
 
-let server = app.listen(3000, function () {
+let server = app.listen(3000, () => {
     let host = server.address().address;
     host = (host === '::' ? 'localhost' : host);
     let port = server.address().port;
@@ -65,7 +106,26 @@ let server = app.listen(3000, function () {
     log('listening at http://%s:%s', host, port);
 });
 
+function processLightStateChange(data, done) {
+    if (!data.lightId || !data.color) {
+        return done(new Error('lightId and color are required!'));
+    }
+    hueService.setLightToColor(data.lightId, data.color);
+    log(`Processing job, set light with ID:${data.lightId} to color: `);
+    log(data.color);
+    done();
+}
+
 async function getTreeStatus() {
     let status = await hueService.getLightColor(2);
     return status;
 }
+
+function validateApiKey(key: string) {
+    if (config.apiKeys.indexOf(key) >= 0) {
+        return true;
+    }
+    return false;
+}
+
+hueService.getLightStatus();
